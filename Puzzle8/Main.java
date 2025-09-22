81ce7723638d09093ba87491b2194146fd5a8c59
Dylan <62079853+DBNines@users.noreply.github.com>
2025-09-22 21:25:18 +0000

    Seeding of inital node

--- Puzzle8/Main.java
+++ Puzzle8/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Test world!");
         Node testObj = new Node();
         testObj.initGame();
         testObj.printState();
--- Puzzle8/Node.java
+++ Puzzle8/Node.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
 public class Node {
     public int[][] gameState;
 
@@ -6,17 +10,18 @@ public class Node {
     }
 
     public void initGame() {
-        gameState[0][0] = 1;
-        gameState[0][1] = 2;
-        gameState[0][2] = 3;
-        gameState[1][0] = 4;
-        gameState[1][1] = 0;
-        gameState[1][2] = 5;
-        gameState[2][0] = 6;
-        gameState[2][1] = 7;
-        gameState[2][2] = 8;
+        Integer[] values = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        int counter = 0;
+        Collections.shuffle(Arrays.asList(values), new Random(29234234));
+        for (int i = 0; i < gameState.length; i++) {
+            for (int j = 0; j < gameState[i].length; j++) {
+                gameState[i][j] = values[counter];
+                counter++;
+            }
+        }
     }
 
+    // Prints the current state of the game in a 3x3 grid
     public void printState() {
         for (int[] row : gameState) {
             for (int positionNumber : row) {
@@ -24,5 +29,45 @@ public class Node {
             }
             System.out.println('\n' + "------");
         }
+    }
+
+    // Checks if the empty spot is in the top row
+    public boolean isEmptyInTopRow() {
+        for (int position : gameState[0]) {
+            if (position == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Checks if the empty spot is in the bottom row
+    public boolean isEmptyInBottomRow() {
+        for (int position : gameState[2]) {
+            if (position == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Checks if the empty spot is in the left column
+    public boolean isEmptyInLeftCol() {
+        for (int[] row : gameState) {
+            if (row[0] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Checks if the empty spot is in the right column
+    public boolean isEmptyInRightCol() {
+        for (int[] row : gameState) {
+            if (row[2] == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
